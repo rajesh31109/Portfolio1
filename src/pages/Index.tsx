8158e50daecc091be6337b11fcbd99aa63b86611
rajesh <rajeshpulluri333@gmail.com>
2025-12-27 05:16:55 +0530

    chore: ensure Footer removal is complete

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,6 @@ import { AboutSection } from '@/components/AboutSection';
 import { InternshipSection } from '@/components/InternshipSection';
 import { ProjectsSection } from '@/components/ProjectsSection';
 import { ContactSection } from '@/components/ContactSection';
-import { Footer } from '@/components/Footer';
 
 const Index = () => {
   return (
@@ -17,7 +16,6 @@ const Index = () => {
         <ProjectsSection />
         <ContactSection />
       </main>
-      <Footer />
     </div>
   );
 };
